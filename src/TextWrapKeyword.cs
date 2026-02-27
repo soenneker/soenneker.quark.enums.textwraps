985855abc86f0c36af9b27aef43b9140429e6aeb
Jake Soenneker <jake@soenneker.com>
2026-02-27 08:24:51 -0600

    Deps

--- src/TextWrapKeyword.cs
+++ src/TextWrapKeyword.cs
@@ -6,10 +6,9 @@ namespace Soenneker.Quark.Enums;
 /// Typed values for the CSS text-wrap property in .NET enumeration form.
 /// </summary>
 [EnumValue<string>]
+[IncludeEnumValues(typeof(GlobalKeyword))]
 public sealed partial class TextWrapKeyword
 {
     public static readonly TextWrapKeyword Wrap = new("wrap");
     public static readonly TextWrapKeyword NoWrap = new("nowrap");
-
-    public static implicit operator TextWrapKeyword(GlobalKeyword style) => new(style.Value);
 }
