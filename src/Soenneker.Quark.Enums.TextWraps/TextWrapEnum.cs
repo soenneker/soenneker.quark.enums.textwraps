b07fe3313ad97c19003e1d099a6f5abb1fe73ab9
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:17 -0500

    xml commenting

--- src/Soenneker.Quark.Enums.TextWraps/TextWrapEnum.cs
+++ src/Soenneker.Quark.Enums.TextWraps/TextWrapEnum.cs
@@ -2,10 +2,22 @@ using Soenneker.Gen.EnumValues;
 
 namespace Soenneker.Quark;
 
+/// <summary>
+/// Represents the text wrap enum.
+/// </summary>
 [EnumValue<string>]
 public sealed partial class TextWrapEnum
 {
+    /// <summary>
+    /// The wrap.
+    /// </summary>
     public static readonly TextWrapEnum Wrap = new("text-wrap");
+    /// <summary>
+    /// The balance.
+    /// </summary>
     public static readonly TextWrapEnum Balance = new("text-balance");
+    /// <summary>
+    /// The pretty.
+    /// </summary>
     public static readonly TextWrapEnum Pretty = new("text-pretty");
 }
